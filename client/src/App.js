24594270e3fb9a491da0e718ec5729034a26e64c
theSherwood <adam.the.sherwood@gmail.com>
2019-07-31 17:55:07 +0100

    init socket ui interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import io from "socket.io-client";
 
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import Home from "./components/home/Home";
-
-import "./App.css";
+export const socket = io.connect("http://localhost:5000");
 
 export default function App() {
+  const [thought, setThought] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  socket.on("error", err => {
+    setError(err);
+  });
+
+  socket.on("loading", () => {
+    setLoading(true);
+  });
+
+  socket.on("thought", thought => {
+    setThought(thought);
+    setLoading(false);
+  });
+
+  function handleClick() {
+    socket.emit("get_thought");
+  }
+
   return (
-    <div
-      className="App"
-      style={{
-        minHeight: "100vh",
-        position: "relative",
-        paddingBottom: "100px"
-      }}
-    >
-      <Header />
-      <Home />
-      <Footer />
+    <div>
+      {thought.hasOwnProperty("currentThought") ? (
+        <>
+          <p>{thought.currentThought}</p>
+          <p>{thought.name}</p>
+          <p>{thought.currentBeer}</p>
+          <img src={thought.daydream} alt="daydream" />
+        </>
+      ) : null}
+      {loading ? <p>Loading...</p> : null}
+      <button onClick={handleClick} disabled={loading}>
+        Read a Mind
+      </button>
     </div>
   );
 }
